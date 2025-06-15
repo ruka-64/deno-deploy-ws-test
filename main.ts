7863b1c9ffbf27bcb58506000c4ff6d2bc58f26d
ruka-64 <202770393+ruka-64@users.noreply.github.com>
2025-06-15 13:33:49 +0900

    done

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { Context, Hono } from 'hono';
-
-const app = new Hono();
-
-app.get('/', (c: Context) => {
-  return c.text('Hello Hono!');
-});
-
-Deno.serve(app.fetch);
---
+++ main.tsx
@@ -0,0 +1,47 @@
+import { Hono } from 'hono';
+import { html } from 'hono/html';
+
+import { upgradeWebSocket } from 'hono/deno';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.html(
+    <html>
+      <head>
+        <meta charset='UTF-8' />
+      </head>
+      <body>
+        <div id='now-time'></div>
+        {html`
+          <script>
+            const ws = new WebSocket('ws://localhost:8000/ws');
+            const $nowTime = document.getElementById('now-time');
+            ws.onmessage = (event) => {
+              $nowTime.textContent = event.data;
+            };
+          </script>
+        `}
+      </body>
+    </html>
+  );
+});
+
+app.get(
+  '/ws',
+  upgradeWebSocket(() => {
+    // deno-lint-ignore no-explicit-any
+    let intervalId: any;
+    return {
+      onOpen(_event, ws) {
+        intervalId = setInterval(() => {
+          ws.send(new Date().toString());
+        }, 1000);
+      },
+      onClose() {
+        clearInterval(intervalId);
+      },
+    };
+  })
+);
+Deno.serve(app.fetch);
